f9eec04682a21d11bf3654712ee53721acbcdf0f
juniorjj13 <_junior@live.com>
2022-06-26 16:09:54 +0100

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
-import * as React from "react"
-
+import * as React from "react";
 
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
       <h1>Berlin 2022</h1>
-      <h2>asd</h2>      
+      <h2>Cristobal author</h2>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
