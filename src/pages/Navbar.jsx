d6cb56e7a194c13736eae21ecd982cac2185261a
Ben <kyutheoriginal@gmail.com>
2022-07-07 14:04:56 +0200

    best navbar in the world

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -3,17 +3,24 @@ import { Link } from "gatsby"
 
 export const Navbar = () => {
     return (
-        <nav className="navbar">  
+        <nav className="navbar">
+            <div class="logo">
+                <Link to="/">
+                    <img src="nav.jpg" width="40px"/>
+                </Link>
+                
+            </div>
+            
             <div className="navButton">
                 <div className="navi"></div>
                 <div className="navi"></div>
                 <div className="navi"></div>
                 <div className="navi"></div>                
             </div>
+            
             <div className="dropdown-content">
-                <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
+                <Link to="/contact">Video</Link>
                 <Link to="/berlin">Berlin Album</Link>  
             </div>     
     </nav> 
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Navbar } from "./Navbar";
+
+
+
 
 
 export default function HelloWorld() {
     return (
         <div>
+            <Navbar />
             <h1>Hello Xbal</h1>
         </div>
     )
