46a3367d182df0961473439fec135a2fa2a836cd
Cristobal <cris.ehmann@tintenfrei.com>
2022-07-13 17:26:31 +0200

    more ununsed stuff deleted

--- src/pages/index.js
+++ src/pages/index.js
@@ -73,14 +73,6 @@ const exp = [
 const imgUrl =
   "https://images.unsplash.com/photo-1655821888788-6107699e1700?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=683&q=80";
 
-// const Pin = (props) => {
-//   return (
-//     <div className="pin">
-//       <div className="pin-content"> {props.value}</div>
-     
-//     </div>
-//   );
-// };
 
 const Card = (props) => {
   const stylingClass = props.order_id % 2 !== 0 ? "left" : "right";
